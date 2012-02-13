f6dd7feccca5beb98f6fc4347ab82955348cebf0
Matt Beedle <mattbeedle@googlemail.com>
2012-02-13 18:10:39 +0100

    Playing around with rack-cache

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,4 +64,8 @@ Jbb::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
+
+  $cache = Dalli::Client.new
+  config.middleware.insert_before Rack::Lock, Rack::Cache, metastore: $cache,
+    entitystore: $cache, allow_reload: false
 end
