da15a3cfa86c24c8416600864344aeab874cd2bb
Matt Beedle <mattbeedle@googlemail.com>
2012-02-13 17:20:01 +0100

    cache platform index page

--- app/controllers/platforms_controller.rb
+++ app/controllers/platforms_controller.rb
@@ -1,6 +1,8 @@
 class PlatformsController < ApplicationController
   respond_to :json
 
+  caches_page :index
+
   expose(:platforms) do
     Platform.published.includes([
       :employment_types, :kinds, :occupational_fields,
