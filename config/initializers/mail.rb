4466eba66c5204e3a5b77219cb46378a5be592db
Matt Beedle <mattbeedle@googlemail.com>
2012-02-22 19:04:31 +0100

    Use authsmtp instead

--- config/initializers/mail.rb
+++ config/initializers/mail.rb
@@ -1,11 +1,10 @@
-if Rails.env.production?
-  ActionMailer::Base.smtp_settings = {
-    :address        => 'smtp.sendgrid.net',
-    :port           => '587',
-    :authentication => :plain,
-    :user_name      => ENV['SENDGRID_USERNAME'],
-    :password       => ENV['SENDGRID_PASSWORD'],
-    :domain         => 'heroku.com'
-  }
+if Rails.env.production? or Rails.env.staging?
   ActionMailer::Base.delivery_method = :smtp
+  ActionMailer::Base.smtp_settings = {
+    :address => 'mail.authsmtp.com',
+    :port => 23,
+    :authentication => :plain,
+    :user_name => "ac34526",
+    :password => "wxxn7zqbb"
+  }
 end
