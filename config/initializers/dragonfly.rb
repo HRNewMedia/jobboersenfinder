084028ffb7332e9e21fe6055196eafbe455a6646
Hans Hasselberg <me@hans.io>
2014-02-09 10:59:48 +0100

    fix aws creds.

--- config/initializers/dragonfly.rb
+++ config/initializers/dragonfly.rb
@@ -8,8 +8,8 @@ if Rails.env.production?
 
   app.datastore.configure do |c|
     c.bucket_name = 'jobboersenfinder'
-    c.access_key_id = 'AKIAJG5WE2U7R3KOUWHQ'
-    c.secret_access_key = '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'
+    c.access_key_id = 'AKIAIGI6BESDVCGGHK6Q'
+    c.secret_access_key = '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'
     # c.region = 'eu-west-1'                        # defaults to 'us-east-1'
     # c.storage_headers = {'some' => 'thing'}       # defaults to {'x-amz-acl' => 'public-read'}
     # c.url_scheme = 'https'                        # defaults to 'http'
